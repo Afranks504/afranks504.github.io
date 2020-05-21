47691f2bee92769962da6ea233d5d5f918ef5533
afranks504 <anthonyfranks504@gmail.com>
2020-05-21 11:31:34 -0500

    added template lits

--- src/components/GetToken.js
+++ src/components/GetToken.js
@@ -5,18 +5,14 @@ class GetToken extends Component {
 
     tokenRequestUrl() {
         const apiUrl = 'https://accounts.spotify.com/authorize';
-        const clientId = '01dde9168baf4375afca1ab6390abf40';
+        const clientId = 'aed1fb11b28a4236aafa2241b827155f';
         const scopes = ['playlist-modify-public'];
         //Don't want to pass parameters or hashes to spotify api
         const bareUrl = window.location.protocol + '//' + window.location.host + window.location.pathname;
         const redirectUri = encodeURIComponent(bareUrl);
         const responseType = 'token';   
         const requestUrl = 
-            apiUrl +
-            '?client_id=' + clientId +
-            '&redirect_uri=' + redirectUri +
-            '&response_type=' + responseType +
-            '&scope=' + encodeURIComponent(scopes)
+            `${apiUrl}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=${encodeURIComponent(scopes)}`
         ;     
         return requestUrl;
     }
