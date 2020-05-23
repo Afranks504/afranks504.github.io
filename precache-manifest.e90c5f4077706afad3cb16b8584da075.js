7c578a22a39f464b9e574fff1cee239f19f3a59c
afranks504 <anthonyfranks504@gmail.com>
2020-05-22 20:20:05 -0500

    Updates

---
+++ precache-manifest.e90c5f4077706afad3cb16b8584da075.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa4cd484cde93ffbaf14008921a2ddfe",
+    "url": "/Afranks504/Afranks504.github.io/index.html"
+  },
+  {
+    "revision": "2fa9b5a721a82d3d6e06",
+    "url": "/Afranks504/Afranks504.github.io/static/css/2.60eec0d8.chunk.css"
+  },
+  {
+    "revision": "d4ef8edb78650b81bdaf",
+    "url": "/Afranks504/Afranks504.github.io/static/css/main.0771b9eb.chunk.css"
+  },
+  {
+    "revision": "2fa9b5a721a82d3d6e06",
+    "url": "/Afranks504/Afranks504.github.io/static/js/2.22a18cab.chunk.js"
+  },
+  {
+    "revision": "d4ef8edb78650b81bdaf",
+    "url": "/Afranks504/Afranks504.github.io/static/js/main.c68edad2.chunk.js"
+  },
+  {
+    "revision": "f7c0594d6746455dffa8",
+    "url": "/Afranks504/Afranks504.github.io/static/js/runtime~main.e5d7fe3c.js"
+  }
+]);
